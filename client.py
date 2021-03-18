ac850a89469d71f3263f9d90bed6f60e3c5a8b88
Mari Carmen <marllacar1@alum.us.es>
2021-03-18 17:26:20 +0100

    Cambio funcion Blake2s -> Blake2b

--- client.py
+++ client.py
@@ -1,30 +1,27 @@
-import secrets
-import hashlib
-import io
-import file_server
+import secrets, hashlib, io, argon2, file_server
 
-
-def generate_token():
+def generate_token(): 
     # Devolvemos un token en hexadecimal con un tamaño de 256 bits (32 bytes).
-    return secrets.token_hex(32)
-
+    token = secrets.token_hex(64)
+    print(token)
+    return token
 
 def hash_file(file):
-    # Creamos el objeto hash, en nuestro caso usaremos Blake2. En concreto usaremos Blake2s que nos devolverá un digest con un tamaño de 256 bits.
-    blake2s = hashlib.blake2s()
+    # Creamos el objeto hash, en nuestro caso usaremos Blake2. En concreto usaremos Blake2b que nos devolverá un digest con un tamaño de 256 bits
+    hash_file = hashlib.blake2b()
 
     # Para optimizar la lectura del fichero obtenemos el tamaño de buffer del sistema.
     buffer_size = io.DEFAULT_BUFFER_SIZE
-
+    
     # Abrimos el archivo para leerlo en modo binario e iteramos con un tamaño de bloque = buffer_size actualizando nuestro objeto hash.
     with open(file, 'rb') as f:
         for chunk in iter(lambda: f.read(buffer_size), b""):
-            blake2s.update(chunk)
-
+            hash_file.update(chunk)
+    
     # Devolvemos la representación del hash en formato hexadecimal.
-    return blake2s.hexdigest()
-
-
+    print(hash_file.hexdigest())
+    return (file, hash_file.hexdigest())
+    
 def challenge(token, hash_file):
     # Convertimos el token y el hash del archivo de hexadecimal a integer mediante la función auxiliar 'hex_to_int()'.
     token_int = hex_to_int(token)
@@ -32,14 +29,16 @@ def challenge(token, hash_file):
 
     # Para asegurarnos que la operación módulo entre ambos valores es un resultado distinto a los mismos, comprobamos que valor es mayor.
     if hash_file_int > token_int:
+        print(hash_file_int % token_int)
         return hash_file_int % token_int
     else:
+        print(token_int % hash_file_int)
         return token_int % hash_file_int
 
-
 def hex_to_int(hex_value):
-    int_value = int(hex_value, base=16)
+    int_value = int(hex_value, base = 16)
     return int_value
+
 
 def check_file_integrity(filepath):
     token = generate_token()
@@ -53,3 +52,6 @@ def check_file_integrity(filepath):
             print("El archivo " + filepath + " es correcto.")
         else:
             print("El archivo " + filepath + " no es correcto: el MAC obtenido en el cliente no es igual al obtenido en el servidor.")
+
+
+
